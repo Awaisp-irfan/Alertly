e101d0d85de34a6d0032f39adfcbda6c0fe2f503
awaisp-irfan <irfanmaqsood.im123@gmail.com>
2024-09-20 02:52:20 +0500

    script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,6 @@ console.log('hello world');
 console.log('hello world');
 console.log('hello world');
 console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
